8ab1c2a1281b33f5c4dd1bbb50cc4eb8a9d351c6
donnchao <donnchao@outlook.com>
2020-12-15 08:37:18 +0800

    4-11 useClickOutside

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="dropdown">
+  <div class="dropdown" ref="dropdownRef">
     <a
       class="btn btn-outline-light my-2 dropdown-toggle"
       href="#"
       @click.prevent="toggleOpen"
       >{{ title }}</a
     >
-    <ul class="dropdown-menu" :style="{ display: 'block' }" v-if="isOpen">
+    <ul class="dropdown-menu" 
+      :style="{ display: 'block' }" 
+      v-if="isOpen"
+      >
       <slot></slot>
     </ul>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref,onMounted, onUnmounted,watch } from "vue";
+import useClickOutside from '../scripts/useClickOutside';
 export default defineComponent({
   name: "Dropdown",
   props: {
@@ -26,9 +30,17 @@ export default defineComponent({
     const toggleOpen = () => {
       isOpen.value = !isOpen.value;
     };
+    const dropdownRef = ref<null | HTMLElement>(null);
+    const isClickedOutside = useClickOutside(dropdownRef);
+    watch(isClickedOutside, () => {
+      if (isOpen.value && isClickedOutside.value) {
+        isOpen.value = false;
+      }
+    })
     return {
       isOpen,
       toggleOpen,
+      dropdownRef
     };
   },
 });
---
+++ src/scripts/useClickOutside.ts
@@ -0,0 +1,23 @@
+import { ref, onMounted, onUnmounted, Ref } from 'vue';
+
+const useClickOutside = (elementRef: Ref<null | HTMLElement>) => {
+  const isClickedOutside = ref(false);
+  const handler = (e: MouseEvent) => {
+    if (elementRef.value) {
+      if (elementRef.value.contains(e.target as HTMLElement)) {
+        isClickedOutside.value = false;
+      } else {
+        isClickedOutside.value = true;
+      }
+    }
+  }
+  onMounted(() => {
+    document.addEventListener('click', handler);
+  });
+  onUnmounted(() => {
+    document.removeEventListener('click', handler);
+  });
+  return isClickedOutside;
+}
+
+export default useClickOutside;
