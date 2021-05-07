fafd3db72bc02a54d6b6df308fb641cb5eb45ffd
董超139 <donnchao@outlook.com>
2021-05-07 22:38:10 +0800

    添加axios

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue'
+import App from './App.vue';
+import axios from 'axios';
 import router from "@/router";
 import store from "@/store";
+
+axios.defaults.baseURL = 'http://apis.imooc.com/api';
+axios.interceptors.request.use(config => {
+    config.params = {...config.params, icode: '15AC610A894E4801'};
+    return config;
+})
+axios.get('/columns').then(res => {
+    console.log(res.data);
+});
 
 const app = createApp(App);
 app.use(router);
