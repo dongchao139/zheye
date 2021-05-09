c468aae0aa9fb32690c94748d105674ba6c209c1
董超139 <donnchao@outlook.com>
2021-05-09 20:28:59 +0800

    const ICODE

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,16 @@ import store from "@/store";
 
 axios.defaults.baseURL = 'http://apis.imooc.com/api';
 axios.interceptors.request.use(config => {
+    const ICODE = "15AC610A894E4801";
     // get 请求，添加到 url 中
-    config.params = { ...config.params, icode: '15AC610A894E4801' }
+    config.params = { ...config.params, icode: ICODE }
     // 其他请求，添加到 body 中
     // 如果是上传文件，添加到 FormData 中
     if (config.data instanceof FormData) {
-        config.data.append('icode', '15AC610A894E4801')
+        config.data.append('icode', ICODE)
     } else {
         // 普通的 body 对象，添加到 data 中
-        config.data = { ...config.data, icode: '15AC610A894E4801' }
+        config.data = { ...config.data, icode: ICODE }
     }
     return config
 })
